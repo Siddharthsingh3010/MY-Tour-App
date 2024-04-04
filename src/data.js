a422bef8a832851d1d8da42c58e0f53dc87a9554
Siddharthsingh Suryawnshi <160884047+Siddharthsingh3010@users.noreply.github.com>
2024-04-04 18:32:26 +0530

    Add files via upload
    # Tour App
    
    ## Introduction
    Welcome to our tour app! This application provides a platform for users to discover and explore various tours and travel destinations. Whether you're a traveler looking for your next adventure or a tour guide wanting to showcase your services, our app has you covered.
    
    ## Installation
    To get started with our tour app, follow these steps:
    1] Run on vsCode terminal : npm i
    2] After : npm start

---
+++ src/data.js
@@ -0,0 +1,46 @@
+const data = [
+	{
+		id: 1,
+		name: "Agra",
+		info: "Agra is a city in northern India, known for the iconic Taj Mahal.Agra is a city on the banks of the Yamuna river in the Indian state of Uttar Pradesh, about 230 kilometres south-east of the national capital Delhi and 330 km west of the state capital Lucknow.",
+		image: "https://cdn.britannica.com/86/170586-050-AB7FEFAE/Taj-Mahal-Agra-India.jpg",
+		price: "14,248 for 4 Days/3 Nights	",
+	},
+	{
+		id: 2,
+		name: "Jaipur",
+		info: `Jaipur is the capital city of the Indian state of Rajasthan, known for its stunning pink-hued buildings and palaces. The city is also known as the "Pink City" due to the color of many of its buildings. Jaipur is famous for its intricate textiles, colorful handicrafts, and delicious cuisine.`,
+		image: "https://media.cnn.com/api/v1/images/stellar/prod/180221121711-victor-cheng-pink-city.jpg?q=w_1110,c_fill",
+		price: "14,248 for 4 Days/3 Nights	",
+	},
+	{
+		id: 3,
+		name: "Goa",
+		info: " Goa is a state in western India with coastlines stretching along the Arabian Sea. Its long history as a Portuguese colony prior to 1961 is evident in its preserved 17th-century churches and the area’s tropical spice plantations. Goa is also known for its beaches.",
+		image: "https://assets.cntraveller.in/photos/65169715f1f1534fc4e0f24d/4:3/w_1640,h_1230,c_limit/W%20Goa.jpg",
+		price: "24,131 for 4 Days/3 Nights	",
+	},
+	{
+		id: 4,
+		name: "Varanasi",
+		info: "Varanasi is a city in the northern Indian state of Uttar Pradesh dating to the 11th century B.C. Regarded as the spiritual capital of India, the city draws Hindu pilgrims who bathe in the Ganges River’s sacred waters and perform funeral rites. Along the city's winding streets are some 2,000 temples, including Kashi Vishwanath, the “Golden Temple,” dedicated to the Hindu god Shiva.",
+		image: "https://media-cdn.tripadvisor.com/media/photo-c/1280x250/0a/be/17/41/caption.jpg",
+		price: "31,095 for6 Days/5 Nights	 ",
+	},
+	{
+		id: 5,
+		name: "Ooty",
+		info: "Ooty is a resort town in the Western Ghats mountains, in southern India's Tamil Nadu state. Founded as a British Raj summer resort. Other reminders of its colonial past include Stone House, a 19th-century residence. ",
+		image: "https://www.abhibus.com/blog/wp-content/uploads/2023/05/Top-20-Places-to-Visit-in-Ooty-in-2023-That-You-Never-Miss-in-Lifetime-scaled.jpg",
+		price: "37,035 for 6 Days/5 Nights	",
+	},
+	{
+		id: 6,
+		name: "Mumbai",
+		info: "Mumbai (formerly called Bombay) is a densely populated city on India’s west coast. A financial center, it's India's largest city. On the Mumbai Harbour waterfront stands the iconic Gateway of India stone arch, built by the British Raj in 1924. Offshore, nearby Elephanta Island holds ancient cave temples dedicated to the Hindu god Shiva.",
+		image: "https://lp-cms-production.imgix.net/image_browser/Mumbai_nightlife_S.jpg",
+		price: "17,049 4 Days/3 Nights",
+	},
+];
+
+export default data;
